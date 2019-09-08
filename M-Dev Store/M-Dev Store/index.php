6578967b8131563ce5957d6ea843181d34053f25
sakun chamikara <43289471+sakunchamikara@users.noreply.github.com>
2019-09-08 15:10:51 +0530

    Add files via upload
    first commit

---
+++ M-Dev Store/M-Dev Store/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>M-dev-store</title>
+    <link rel="stylesheet" href="styles/bootstrap-337.min.css">
+    <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles/style.css">
+
+</head>
+<body>
+    <!--top begin-->
+    <div id="top">
+        <div class="container">    <!--top container-->
+            <din class="col-md-6 offer"><!--top col-md-6 offer-->
+                <a href="" class="btn btn-success btn-sm">welcom</a>
+                <a href="checkout.php">4 items in your cart | Total price is $300</a>
+            </din><!--end col-md-6 offer-->
+            <div class="col-md-6"><!--top col-md-6 -->
+                <ul class="menu"><!--top cmenu -->
+                    <li>
+                        <a href="customer_register.php">Register</a>
+                    </li>
+                    <li>
+                        <a href="checkout.php">My Account</a>
+                    </li>
+                    <li>
+                        <a href="cart.php">Go to cart</a>
+                    </li>
+                    <li>
+                        <a href="checkout.php">Login</a>
+                    </li>
+                </ul><!--end cmenu -->
+            </div><!--end col-md-6 -->
+        </div><!-- container end-->
+    </div>
+    <!--top end-->
+
+    <script src="js/jquery-331.min.js"></script>
+    <script src="js/bootstrap-337.min.js"></script>
+</body>
+</html>
