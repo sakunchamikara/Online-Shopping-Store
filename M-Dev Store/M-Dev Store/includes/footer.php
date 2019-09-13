203e61407b02cbccd63930c5dc56f24e3f7a1a3b
sakunchamikara <2017cs014@stu.ucsc.cmb.ac.lk>
2019-09-13 11:09:31 +0530

    create footer

---
+++ M-Dev Store/M-Dev Store/includes/footer.php
@@ -0,0 +1,111 @@
+<div id="footer"><!-- footer begin -->
+    <div class="container"><!-- container begin -->
+        <div class="row"><!-- row begin -->
+            <div class="col-sm-6 col-md-3"><!-- col-sm-6 col-md-3 begin -->
+
+                <h4>Pages</h4>
+
+                <ul>
+                    <li><a href="cart.php">Shopping Cart</a></li>
+                    <li><a href="contact.php">Contact Us</a></li>
+                    <li><a href="shop.php">Shop</a></li>
+                    <li><a href="checkout.php">My Account</a></li>
+                </ul>
+
+                <h4>User Section</h4>
+
+                <ul>
+                    <li><a href="checkout.php">Login</a></li>
+                    <li><a href="customer_register.php">Register</a></li>
+                </ul>
+
+                <hr class="hidden-md hidden-lg hidden-sm">
+
+            </div><!-- col-sm-6 col-md-3 end -->
+
+            <div class="com-sm-6 col-md-3"><!-- col-sm-6 col-md-3 begin -->
+
+                <h4>Top Products Catogories</h4>
+
+                <ul>
+                    <li><a href="#">Jacket</a></li>
+                    <li><a href="#">Accesssories</a></li>
+                    <li><a href="#">Coat</a></li>
+                    <li><a href="#">Shoes</a></li>
+                    <li><a href="#">Tshirts</a></li>
+                </ul>
+
+                <hr class="hidden-md hidden-lg">
+
+            </div><!-- col-sm-6 col-md-3 end -->
+
+            <div class="com-sm-6 col-md-3"><!-- col-sm-6 col-md-3 begin -->
+
+                <h4>Find Us:</h4>
+
+                <p>
+                    <strong>M Dev midea inc.</strong>
+                    <br>Ciber
+                    <br>Ciracus
+                    <br>0767035245
+                    <br>sakun@gmail.com
+                    <br><strong>Sakun</strong>
+                </p>
+
+                <a href="contact.php">Check Our Contact Page</a>
+                
+                <hr class="hidden-md hidden-lg">
+
+            </div><!-- col-sm-6 col-md-3 end -->
+            
+            <div class="com-sm-6 col-md-3"><!-- col-sm-6 col-md-3 begin -->
+
+                <h4>Get the NEWS</h4>
+
+                <p class="text-muted">
+                    Subscribe to get latest news from us
+                </p>
+
+                <form action="" method="post">
+                    <div class="input-group">
+                        <input type="text" class="form-control" name="email">
+                        <span class="input-group-btn">
+                            <input type="submit" value="subscribe" class="btn btn-default">
+                        </span>
+                    </div>
+                </form>
+                <hr>
+                <h4>Keep In Touch</h4>
+    
+                <p class="social">
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                    <a href="#" class="fa fa-instagram"></a>
+                    <a href="#" class="fa fa-google-plus"></a>
+                    <a href="#" class="fa fa-envelope"></a>
+                </p>
+
+            </div><!-- col-sm-6 col-md-3 end -->
+
+
+        </div><!-- row end -->
+    </div><!-- container end -->
+</div><!-- footer end -->
+
+<div id="copyright">
+    <div class="container">
+        <div class="col-md-6">
+
+            <p class="pull-left">
+                <strong>&copy; 2018 M Dev Store All Rights Recerve</strong>
+            </p>
+
+        </div>
+        
+        <div class="col-md-6">
+
+            <p class="pull-right">Theme By: <a href="#">Sakun</a></p>
+
+        </div>
+    </div>
+</div>
--- M-Dev Store/M-Dev Store/index.php
+++ M-Dev Store/M-Dev Store/index.php
@@ -591,6 +591,12 @@
 
     </div><!-- container end-->
 
+    <?php
+
+    include("includes/footer.php");
+
+    ?>
+
     <script src="js/jquery-331.min.js"></script>
     <script src="js/bootstrap-337.min.js"></script>
 </body>
