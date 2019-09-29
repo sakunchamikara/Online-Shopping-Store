bdf80a748b98c02fbbbf87f63c264bb4696e306e
sakunchamikara <2017cs014@stu.ucsc.cmb.ac.lk>
2019-09-29 13:31:35 +0530

    login form and payment option

--- M-Dev Store/M-Dev Store/customer/includes/header.php
+++ M-Dev Store/M-Dev Store/customer/includes/header.php
@@ -1,9 +1,49 @@
-<?php
+<?php 
+
 session_start();
+
 include("includes/db.php");
 include("functions/functions.php");
 
-?> 
+?>
+
+<?php 
+
+if(isset($_GET['pro_id'])){
+    
+    $product_id = $_GET['pro_id'];
+    
+    $get_product = "select * from products where product_id='$product_id'";
+    
+    $run_product = mysqli_query($con,$get_product);
+    
+    $row_product = mysqli_fetch_array($run_product);
+    
+    $p_cat_id = $row_product['p_cat_id'];
+    
+    $pro_title = $row_product['product_title'];
+    
+    $pro_price = $row_product['product_price'];
+    
+    $pro_desc = $row_product['product_desc'];
+    
+    $pro_img1 = $row_product['product_img1'];
+    
+    $pro_img2 = $row_product['product_img2'];
+    
+    $pro_img3 = $row_product['product_img3'];
+    
+    $get_p_cat = "select * from product_categories where p_cat_id='$p_cat_id'";
+    
+    $run_p_cat = mysqli_query($con,$get_p_cat);
+    
+    $row_p_cat = mysqli_fetch_array($run_p_cat);
+    
+    $p_cat_title = $row_p_cat['p_cat_title'];
+    
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -116,7 +156,7 @@ include("functions/functions.php");
 
                 <a href="cart.php" class="btn btn-primary navbar-btn right"><!--btn navbar-btn primary right begin-->
                     <i class="fa fa-shopping-cart"></i>
-                    <span>4 items in your shopping cart</span>
+                    <span><?php items();?> items in your cart</span>
                 </a><!--btn navbar-btn primary right end-->
                 <div class="navbar-collapse collapse right"><!-- navbar-collapse collapse right finish-->
                     <button class="btn btn-primary navbar-btn" type="button" data-toggle="collapse" data-target="#search"><!-- btn btn-primary navbar-btn begin-->
