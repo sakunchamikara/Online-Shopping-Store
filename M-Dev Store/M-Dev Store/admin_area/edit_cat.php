07293517a1e38580b65655dc2faa6204010a7e5a
sakunchamikara <2017cs014@stu.ucsc.cmb.ac.lk>
2019-12-24 20:55:44 +0530

    Admin Panel Creating - Category n Slider

--- M-Dev Store/M-Dev Store/admin_area/edit_cat.php
+++ M-Dev Store/M-Dev Store/admin_area/edit_cat.php
@@ -10,21 +10,21 @@
 
 <?php 
 
-    if(isset($_GET['edit_p_cat'])){
+    if(isset($_GET['edit_cat'])){
         
-        $edit_p_cat_id = $_GET['edit_p_cat'];
+        $edit_cat_id = $_GET['edit_cat'];
         
-        $edit_p_cat_query = "select * from product_categories where p_cat_id='$edit_p_cat_id'";
+        $edit_cat_query = "select * from categories where cat_id='$edit_cat_id'";
         
-        $run_edit = mysqli_query($con,$edit_p_cat_query);
+        $run_edit = mysqli_query($con,$edit_cat_query);
         
         $row_edit = mysqli_fetch_array($run_edit);
         
-        $p_cat_id = $row_edit['p_cat_id'];
+        $cat_id = $row_edit['cat_id'];
         
-        $p_cat_title = $row_edit['p_cat_title'];
+        $cat_title = $row_edit['cat_title'];
         
-        $p_cat_desc = $row_edit['p_cat_desc'];
+        $cat_desc = $row_edit['cat_desc'];
         
     }
 
@@ -35,7 +35,7 @@
         <ol class="breadcrumb"><!-- breadcrumb begin -->
             <li>
                 
-                <i class="fa fa-dashboard"></i> Dashboard / Edit Product Category
+                <i class="fa fa-dashboard"></i> Dashboard / Edit Category
                 
             </li>
         </ol><!-- breadcrumb finish -->
@@ -48,7 +48,7 @@
             <div class="panel-heading"><!-- panel-heading begin -->
                 <h3 class="panel-title"><!-- panel-title begin -->
                 
-                    <i class="fa fa-pencil fa-fw"></i> Edit Product Category
+                    <i class="fa fa-pencil fa-fw"></i> Edit Category
                 
                 </h3><!-- panel-title finish -->
             </div><!-- panel-heading finish -->
@@ -59,13 +59,13 @@
                     
                         <label for="" class="control-label col-md-3"><!-- control-label col-md-3 begin --> 
                         
-                            Product Category Title 
+                            Category Title 
                         
                         </label><!-- control-label col-md-3 finish --> 
                         
                         <div class="col-md-6"><!-- col-md-6 begin -->
                         
-                            <input value=" <?php echo $p_cat_title; ?> " name="p_cat_title" type="text" class="form-control">
+                            <input value=" <?php echo $cat_title; ?> " name="cat_title" type="text" class="form-control">
                         
                         </div><!-- col-md-6 finish -->
                     
@@ -74,13 +74,13 @@
                     
                         <label for="" class="control-label col-md-3"><!-- control-label col-md-3 begin --> 
                         
-                            Product Category Description 
+                            Category Description 
                         
                         </label><!-- control-label col-md-3 finish --> 
                         
                         <div class="col-md-6"><!-- col-md-6 begin -->
                         
-                            <textarea type='text' name="p_cat_desc" class="form-control"><?php echo $p_cat_desc; ?></textarea>
+                            <textarea type='text' name="cat_desc" class="form-control"><?php echo $cat_desc; ?></textarea>
                         
                         </div><!-- col-md-6 finish -->
                     
@@ -111,19 +111,19 @@
 
           if(isset($_POST['update'])){
               
-              $p_cat_title = $_POST['p_cat_title'];
+              $cat_title = $_POST['cat_title'];
               
-              $p_cat_desc = $_POST['p_cat_desc'];
+              $cat_desc = $_POST['cat_desc'];
               
-              $update_p_cat = "update product_categories set p_cat_title='$p_cat_title',p_cat_desc='$p_cat_desc' where p_cat_id='$p_cat_id'";
+              $update_cat = "update categories set cat_title='$cat_title',cat_desc='$cat_desc' where cat_id='$cat_id'";
               
-              $run_p_cat = mysqli_query($con,$update_p_cat);
+              $run_cat = mysqli_query($con,$update_cat);
               
-              if($run_p_cat){
+              if($run_cat){
                   
-                  echo "<script>alert('Your Product Category Has Been Updated')</script>";
+                  echo "<script>alert('Your Category Has Been Updated')</script>";
                   
-                  echo "<script>window.open('index.php?view_p_cats','_self')</script>";
+                  echo "<script>window.open('index.php?view_cats','_self')</script>";
                   
               }
               
@@ -133,4 +133,4 @@
 
 
 
-<?php } ?>   
+<?php } ?> 
--- M-Dev Store/M-Dev Store/admin_area/insert_slide.php
+++ M-Dev Store/M-Dev Store/admin_area/insert_slide.php
@@ -8,34 +8,12 @@
 
 ?>
 
-<?php 
-
-    if(isset($_GET['edit_p_cat'])){
-        
-        $edit_p_cat_id = $_GET['edit_p_cat'];
-        
-        $edit_p_cat_query = "select * from product_categories where p_cat_id='$edit_p_cat_id'";
-        
-        $run_edit = mysqli_query($con,$edit_p_cat_query);
-        
-        $row_edit = mysqli_fetch_array($run_edit);
-        
-        $p_cat_id = $row_edit['p_cat_id'];
-        
-        $p_cat_title = $row_edit['p_cat_title'];
-        
-        $p_cat_desc = $row_edit['p_cat_desc'];
-        
-    }
-
-?>
-
 <div class="row"><!-- row 1 begin -->
     <div class="col-lg-12"><!-- col-lg-12 begin -->
         <ol class="breadcrumb"><!-- breadcrumb begin -->
             <li>
                 
-                <i class="fa fa-dashboard"></i> Dashboard / Edit Product Category
+                <i class="fa fa-dashboard"></i> Dashboard / Insert Slide
                 
             </li>
         </ol><!-- breadcrumb finish -->
@@ -48,24 +26,24 @@
             <div class="panel-heading"><!-- panel-heading begin -->
                 <h3 class="panel-title"><!-- panel-title begin -->
                 
-                    <i class="fa fa-pencil fa-fw"></i> Edit Product Category
+                    <i class="fa fa-money fa-fw"></i> Insert Slide
                 
                 </h3><!-- panel-title finish -->
             </div><!-- panel-heading finish -->
             
             <div class="panel-body"><!-- panel-body begin -->
-                <form action="" class="form-horizontal" method="post"><!-- form-horizontal begin -->
+                <form action="" class="form-horizontal" method="post" enctype="multipart/form-data"><!-- form-horizontal begin -->
                     <div class="form-group"><!-- form-group begin -->
                     
                         <label for="" class="control-label col-md-3"><!-- control-label col-md-3 begin --> 
                         
-                            Product Category Title 
+                            Slide Name 
                         
                         </label><!-- control-label col-md-3 finish --> 
                         
                         <div class="col-md-6"><!-- col-md-6 begin -->
                         
-                            <input value=" <?php echo $p_cat_title; ?> " name="p_cat_title" type="text" class="form-control">
+                            <input name="slide_name" type="text" class="form-control">
                         
                         </div><!-- col-md-6 finish -->
                     
@@ -74,28 +52,24 @@
                     
                         <label for="" class="control-label col-md-3"><!-- control-label col-md-3 begin --> 
                         
-                            Product Category Description 
+                            Slide Image
                         
                         </label><!-- control-label col-md-3 finish --> 
                         
                         <div class="col-md-6"><!-- col-md-6 begin -->
                         
-                            <textarea type='text' name="p_cat_desc" class="form-control"><?php echo $p_cat_desc; ?></textarea>
+                            <input type="file" name="slide_image" class="form-control">
                         
                         </div><!-- col-md-6 finish -->
                     
                     </div><!-- form-group finish -->
                     <div class="form-group"><!-- form-group begin -->
                     
-                        <label for="" class="control-label col-md-3"><!-- control-label col-md-3 begin --> 
-                        
-                             
-                        
-                        </label><!-- control-label col-md-3 finish --> 
+                        <label for="" class="control-label col-md-3"><!-- control-label col-md-3 begin --></label><!-- control-label col-md-3 finish --> 
                         
                         <div class="col-md-6"><!-- col-md-6 begin -->
                         
-                            <input value="Update" name="update" type="submit" class="form-control btn btn-primary">
+                            <input type="submit" name="submit" value="Submit Now" class="btn btn-primary form-control">
                         
                         </div><!-- col-md-6 finish -->
                     
@@ -109,28 +83,40 @@
 
 <?php  
 
-          if(isset($_POST['update'])){
-              
-              $p_cat_title = $_POST['p_cat_title'];
-              
-              $p_cat_desc = $_POST['p_cat_desc'];
-              
-              $update_p_cat = "update product_categories set p_cat_title='$p_cat_title',p_cat_desc='$p_cat_desc' where p_cat_id='$p_cat_id'";
-              
-              $run_p_cat = mysqli_query($con,$update_p_cat);
-              
-              if($run_p_cat){
-                  
-                  echo "<script>alert('Your Product Category Has Been Updated')</script>";
-                  
-                  echo "<script>window.open('index.php?view_p_cats','_self')</script>";
-                  
-              }
-              
-          }
+    if(isset($_POST['submit'])){
+        
+        $slide_name = $_POST['slide_name'];
+        
+        $slide_image = $_FILES['slide_image']['name'];
+        
+        $temp_name = $_FILES['slide_image']['tmp_name'];
+        
+        $view_slides = "select * from slider";
+        
+        $view_run_slide = mysqli_query($con,$view_slides);
+        
+        $count = mysqli_num_rows($view_run_slide);
+        
+        if($count<4){
+            
+            move_uploaded_file($temp_name,"slides_images/$slide_image");
+            
+            $insert_slide = "insert into slider (slide_name,slide_image) values ('$slide_name','$slide_image')";
+            
+            $run_slide = mysqli_query($con,$insert_slide);
+            
+            echo "<script>alert('Your new slide image has been inserted')</script>";
+            
+            echo "<script>window.open('index.php?view_slides','_self')</script>";
+            
+        }else{
+            
+           echo "<script>alert('You have already inserted 4 slides')</script>"; 
+            
+        }
+        
+    }
 
 ?>
 
-
-
-<?php } ?>   
+<?php } ?>
