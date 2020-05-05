834fb8b304561bbeff9c3ab706d88b7c5128beec
sakunchamikara <2017cs014@stu.ucsc.cmb.ac.lk>
2020-05-05 20:11:58 +0530

    add paypal details

--- M-Dev Store/M-Dev Store/includes/header.php
+++ M-Dev Store/M-Dev Store/includes/header.php
@@ -10,10 +10,10 @@ include("functions/functions.php");
 <?php
 
 if (isset($_GET['pro_id'])) {
-    
+
     $product_id = $_GET['pro_id'];
     $get_product = "select * from products where product_id='$product_id'";
-    $run_product = mysqli_query($con,$get_product);
+    $run_product = mysqli_query($con, $get_product);
     $row_product = mysqli_fetch_array($run_product);
 
     $p_cat_id = $row_product['p_cat_id'];
@@ -25,51 +25,59 @@ if (isset($_GET['pro_id'])) {
     $pro_img3 = $row_product['product_img3'];
 
     $get_p_cat = "select * from product_categories where p_cat_id='$p_cat_id'";
-    $run_p_cat = mysqli_query($con,$get_p_cat);
+    $run_p_cat = mysqli_query($con, $get_p_cat);
     $row_p_cat = mysqli_fetch_array($run_p_cat);
     $p_cat_title = $row_p_cat['p_cat_title'];
-
 }
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- paypal details begin -->
+    <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- Ensures optimal rendering on mobile devices. -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" /> <!-- Optimal Internet Explorer compatibility -->
+    <!-- end paypal details -->
     <title>sakun chamikara</title>
     <link rel="stylesheet" href="styles/bootstrap-337.min.css">
     <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
     <link rel="stylesheet" href="styles/style.css">
 
 </head>
+
 <body>
     <!--top begin-->
     <div id="top">
-        <div class="container">    <!--top container-->
-            <div class="col-md-6 offer"><!--top col-md-6 offer-->
+        <div class="container">
+            <!--top container-->
+            <div class="col-md-6 offer">
+                <!--top col-md-6 offer-->
                 <a href="" class="btn btn-success btn-sm">
-                    
-                <?php 
-                   
-                   if(!isset($_SESSION['customer_email'])){
-                       
-                       echo "Welcome: Guest";
-                       
-                   }else{
-                       
-                       echo "Welcome: " . $_SESSION['customer_email'] . "";
-                       
-                   }
-                   
-                ?>
+
+                    <?php
+
+                    if (!isset($_SESSION['customer_email'])) {
+
+                        echo "Welcome: Guest";
+                    } else {
+
+                        echo "Welcome: " . $_SESSION['customer_email'] . "";
+                    }
+
+                    ?>
 
                 </a>
                 <a href="checkout.php"><?php items(); ?> items in your cart | Total price is <?php total_price(); ?></a>
-            </div><!--end col-md-6 offer-->
-            <div class="col-md-6"><!--top col-md-6 -->
-                <ul class="menu"><!--top cmenu -->
+            </div>
+            <!--end col-md-6 offer-->
+            <div class="col-md-6">
+                <!--top col-md-6 -->
+                <ul class="menu">
+                    <!--top cmenu -->
                     <li>
                         <a href="customer_register.php">Register</a>
                     </li>
@@ -81,34 +89,40 @@ if (isset($_GET['pro_id'])) {
                     </li>
                     <li>
                         <a href="checkout.php">
-                           
-                           <?php 
-                           
-                           if(!isset($_SESSION['customer_email'])){
-                       
-                                echo "<a href='checkout.php'> Login </a>";
 
-                               }else{
+                            <?php
+
+                            if (!isset($_SESSION['customer_email'])) {
+
+                                echo "<a href='checkout.php'> Login </a>";
+                            } else {
 
                                 echo " <a href='logout.php'> Log Out </a> ";
+                            }
 
-                               }
-                           
-                           ?>
+                            ?>
                         </a>
                     </li>
-                </ul><!--end cmenu -->
-            </div><!--end col-md-6 -->
+                </ul>
+                <!--end cmenu -->
+            </div>
+            <!--end col-md-6 -->
         </div><!-- container end-->
-    </div><!--top end-->
-    
-    <div id="navbar" class="navbar navbar-default"><!-- navbar navbar-default begin-->
-        <div class="container"><!-- container begin-->
-            <div class="navbar-header"><!--navbar-header begin-->
-                <a href="index.php" class="navbar-brand home"><!--navbar-brand home begin-->
+    </div>
+    <!--top end-->
+
+    <div id="navbar" class="navbar navbar-default">
+        <!-- navbar navbar-default begin-->
+        <div class="container">
+            <!-- container begin-->
+            <div class="navbar-header">
+                <!--navbar-header begin-->
+                <a href="index.php" class="navbar-brand home">
+                    <!--navbar-brand home begin-->
                     <img src="images/ecom-store-logo.png" alt="M-dev-store-logo" class="hidden-xs">
                     <img src="images/ecom-store-logo-mobile.png" alt="M-dev-store-logo-mobile" class="visible-xs">
-                </a><!--navbar-brand home end-->
+                </a>
+                <!--navbar-brand home end-->
                 <button class="navbar-toggle" data-toggle="collapse" data-target="#navigation">
                     <span class="sr-only">Toggle Navigation</span>
                     <i class="fa fa-align-justify"></i>
@@ -117,49 +131,59 @@ if (isset($_GET['pro_id'])) {
                     <span class="sr-only">Toggle Search</span>
                     <i class="fa fa-search"></i>
                 </button>
-            </div><!--navbar-header end-->
-            <div class="navbar-collapse collapse" id="navigation"><!--navbar-collapse collapse begin-->
-                
-                <div class="padding-nav"><!--padding-nav begin-->
+            </div>
+            <!--navbar-header end-->
+            <div class="navbar-collapse collapse" id="navigation">
+                <!--navbar-collapse collapse begin-->
 
-                    <ul class="nav navbar-nav left"><!--nav navbar-nav left begin-->
+                <div class="padding-nav">
+                    <!--padding-nav begin-->
 
-                        <li class="<?php if($active=='Home') echo"active"; ?>">
+                    <ul class="nav navbar-nav left">
+                        <!--nav navbar-nav left begin-->
+
+                        <li class="<?php if ($active == 'Home') echo "active"; ?>">
                             <a href="index.php">Home</a>
                         </li>
-                        <li class="<?php if($active=='Shop') echo"active"; ?>">
+                        <li class="<?php if ($active == 'Shop') echo "active"; ?>">
                             <a href="shop.php">Shop</a>
                         </li>
-                        <li class="<?php if($active=='Account') echo"active"; ?>">
+                        <li class="<?php if ($active == 'Account') echo "active"; ?>">
                             <?php
-                            
-                            if(!isset($_SESSION['customer_email'])){
+
+                            if (!isset($_SESSION['customer_email'])) {
 
                                 echo "<a href='checkout.php'>My Account</a>";
-                            }else{
+                            } else {
 
                                 echo "<a href='customer/my_account.php?my_orders'>My Account</a>";
                             }
-                            
+
                             ?>
                         </li>
-                        <li class="<?php if($active=='Cart') echo"active"; ?>">
+                        <li class="<?php if ($active == 'Cart') echo "active"; ?>">
                             <a href="cart.php">Shopping cart</a>
                         </li>
-                        <li class="<?php if($active=='Contact') echo"active"; ?>">
+                        <li class="<?php if ($active == 'Contact') echo "active"; ?>">
                             <a href="contact.php">Contact Us</a>
                         </li>
 
-                    </ul><!--nav navbar-nav left end-->
+                    </ul>
+                    <!--nav navbar-nav left end-->
 
-                </div><!--padding-nav end-->
+                </div>
+                <!--padding-nav end-->
 
-                <a href="cart.php" class="btn btn-primary navbar-btn right"><!--btn navbar-btn primary right begin-->
+                <a href="cart.php" class="btn btn-primary navbar-btn right">
+                    <!--btn navbar-btn primary right begin-->
                     <i class="fa fa-shopping-cart"></i>
                     <span><?php items(); ?> items in your shopping cart</span>
-                </a><!--btn navbar-btn primary right end-->
-                <div class="navbar-collapse collapse right"><!-- navbar-collapse collapse right finish-->
-                    <button class="btn btn-primary navbar-btn" type="button" data-toggle="collapse" data-target="#search"><!-- btn btn-primary navbar-btn begin-->
+                </a>
+                <!--btn navbar-btn primary right end-->
+                <div class="navbar-collapse collapse right">
+                    <!-- navbar-collapse collapse right finish-->
+                    <button class="btn btn-primary navbar-btn" type="button" data-toggle="collapse" data-target="#search">
+                        <!-- btn btn-primary navbar-btn begin-->
 
                         <span class="sr-only">Toggle Search</span>
 
@@ -167,30 +191,35 @@ if (isset($_GET['pro_id'])) {
 
                     </button><!-- btn btn-primary navbar-btn end -->
                 </div><!-- navbar-collapse collapse right end-->
-                
-                <div class="collapse clearfix" id="search"><!-- collapse clearfix begin-->
 
-                    <form method="get" action="result.php" class="navbar-form"><!-- navbar-form begin-->
+                <div class="collapse clearfix" id="search">
+                    <!-- collapse clearfix begin-->
 
-                        <div class="input-group"><!-- input-group begin-->
+                    <form method="get" action="result.php" class="navbar-form">
+                        <!-- navbar-form begin-->
+
+                        <div class="input-group">
+                            <!-- input-group begin-->
 
                             <input type="text" name="user_query" placeholder="Search" required class="form-control">
 
                             <span class="input-group-btn">
 
-                            <button type="submit" name="search" value="search" class="btn btn-primary">
-                                <i class="fa fa-search"></i>
-                            </button>
+                                <button type="submit" name="search" value="search" class="btn btn-primary">
+                                    <i class="fa fa-search"></i>
+                                </button>
 
                             </span>
- 
+
                         </div><!-- input-group end-->
-                    
+
                     </form><!-- navbar-form end-->
 
                 </div><!-- collapse clearfix end-->
 
-            </div><!--navbar-collapse collapse end--> 
+            </div>
+            <!--navbar-collapse collapse end-->
         </div><!-- container end-->
-    
-    </div><!--navbar navbar-default end-->
+
+    </div>
+    <!--navbar navbar-default end-->
--- M-Dev Store/M-Dev Store/payment_options.php
+++ M-Dev Store/M-Dev Store/payment_options.php
@@ -1,41 +1,77 @@
-<div class="box"><!-- box Begin -->
-   
-   <?php 
-    
+<div class="box">
+    <!-- box Begin -->
+
+    <?php
+
     $session_email = $_SESSION['customer_email'];
-    
+
     $select_customer = "select * from customers where customer_email='$session_email'";
-    
-    $run_customer = mysqli_query($con,$select_customer);
-    
+
+    $run_customer = mysqli_query($con, $select_customer);
+
     $row_customer = mysqli_fetch_array($run_customer);
-    
+
     $customer_id = $row_customer['customer_id'];
-    
+
     ?>
-    
-    <h1 class="text-center">Payment Options For You</h1>  
-    
-     <p class="lead text-center"><!-- lead text-center Begin -->
-         
-         <a href="order.php?c_id=<?php echo $customer_id ?>"> Offline Payment </a>
-         
-     </p><!-- lead text-center Finish -->
-     
-     <center><!-- center Begin -->
-         
-        <p class="lead"><!-- lead Begin -->
-            
+
+    <h1 class="text-center">Payment Options For You</h1>
+
+    <p class="lead text-center">
+        <!-- lead text-center Begin -->
+
+        <a href="order.php?c_id=<?php echo $customer_id ?>"> Offline Payment </a>
+
+    </p><!-- lead text-center Finish -->
+
+    <center>
+        <!-- center Begin -->
+
+        <!-- lead Begin -->
+        <!-- <p class="lead">
+
             <a href="#">
-                
+
                 Paypall Payment
-                
                 <img class="img-responsive" src="images/paypall_img.png" alt="img-paypall">
                 
             </a>
-            
-        </p> <!-- lead Finish -->
-         
-     </center><!-- center Finish -->
-    
+
+        </p>  -->
+        <!-- lead Finish -->
+
+        <!-- paypal details begin -->
+        <script src="https://www.paypal.com/sdk/js?client-id=AXdPFk_VFLTHsTArslR4FuV5NQacG8v8_OdaF5KXheAwOBrkW6LpJ-_zlF3CIObFgmXEN0QKe4QWTTq8">
+            // Required. Replace SB_CLIENT_ID with your sandbox client ID.
+        </script>
+        <!-- paypal details end -->
+        <div id="paypal-button-container"></div>
+
+        <script>
+            paypal.Buttons({
+                createOrder: function(data, actions) {
+                    // This function sets up the details of the transaction, including the amount and line item details.
+                    return actions.order.create({
+                        purchase_units: [{
+                            amount: {
+                                value: "<?php total_price(); ?>"
+                            }
+                        }]
+                    });
+                },
+                onApprove: function(data, actions) {
+                    // This function captures the funds from the transaction.
+                    return actions.order.capture().then(function(details) {
+                        // This function shows a transaction success message to your buyer.
+                        alert('Transaction completed by ' + details.payer.name.given_name);
+                    });
+                }
+            }).render('#paypal-button-container');
+            //This function displays Smart Payment Buttons on your web page.
+        </script>
+
+    </center>
+
+    <!-- center Finish -->
+
 </div><!-- box Finish -->
